1335107734913b44b4a5e34fe1aad8de9a1a69cc
MouJ <129665424+SweerItTer@users.noreply.github.com>
2026-03-15 20:22:32 +0800

    update: 修改订阅地址

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ https://cfxr.eu.org/getSub
 `;
 
 let urls = [];
-let subConverter = "lrkxugzxhhyh.ap-northeast-1.clawcloudrun.com"; // 在线订阅转换后端，当前使用自建 subconverter
+let subConverter = "subapi.mouj.dpdns.org"; // 在线订阅转换后端，当前使用自建 subconverter
 let subConfig = "https://raw.githubusercontent.com/SweerItTer/CF-Workers-SUB/refs/heads/confg/ACL4SSR_Online_Full_Mannix.ini"; // 订阅配置文件
 let subProtocol = 'https';
 
